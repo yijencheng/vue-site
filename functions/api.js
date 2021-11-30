2ab3bd5c2056f1c30583ec63918daaed0c4e0531
yijencheng <ivan861118@gmail.com>
2021-11-30 22:55:02 +0800

    init serverless endpoint

---
+++ functions/api.js
@@ -0,0 +1,8 @@
+//path: /.netlify/functions/api
+
+exports.handler = async function(event, context) {
+  return {
+      statusCode: 200,
+      body: JSON.stringify({message: "Hello World"})
+  };
+}
